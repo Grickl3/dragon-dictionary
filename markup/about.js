d078fd3805aca8157a626981173431c51f1220b4
Deacon <Michocan@Carloss-MacBook-Pro.local>
2018-12-27 02:08:10 -0700

    html insertion chugging along.

---
+++ markup/about.js
@@ -0,0 +1,6 @@
+
+
+
+function aboutLoad(a) {
+	a.innerHTML = "<h1>Here we have some About type content!</h1><h3>stuff and things …</h3>";
+};
---
+++ markup/definition.js
@@ -0,0 +1,6 @@
+
+
+
+function definition(a) {
+	a.innerHTML = "<h1>This will be some definition stuff!</h1><h3>All Verbs</h3>";
+};
